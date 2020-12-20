85a6f77528bfaedd482712ef9d657e64e648f782
Aaron Lee <10851022@st.fhjh.tp.edu.tw>
2020-12-20 15:33:11 +0800

    New Method

---
+++ 6440ca32e97ef26a05597c88839e8faef8933635/assets/js/main.js
@@ -0,0 +1,8 @@
+$(".dropdown-item").click(function () {
+    var target = $(this).attr("href");
+    var locate = $(target).offset().top - $(".navbar").outerHeight() + 20;
+    var duration = 1000;
+    $("html, body").stop().animate({
+        scrollTop: locate
+    }, duration);
+});
