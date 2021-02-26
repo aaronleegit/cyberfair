77de4d4c01061bcb2b303e19218b5ddaaa34fdac
Aaron Lee <longyklee@gmail.com>
2021-02-26 21:19:28 +0800

    Teacher Edit I

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
 $('.carousel').carousel({
     interval: 1000 * 3
 });
+/***設定文繞圖與文字齊高***/
+var $l_img = $('img.l-img');
+var $r_img = $('img.r-img');
+$l_img.each(function(){
+	$this = $(this);
+	$parent = $this.parent();
+	if($parent.height() < $this.height()){
+		$parent.height($this.height());
+	}
+})
+
+$r_img.each(function(){
+	$this = $(this);
+	$parent = $this.parent();
+	if($parent.height() < $this.height()){
+		$parent.height($this.height());
+	}
+})
+/******/
